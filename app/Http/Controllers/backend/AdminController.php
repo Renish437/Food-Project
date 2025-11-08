59bcecf620b3fe1b3fde4762e03b14ca6bb21a91
Renish437 <renishsiwakoti437@gmail.com>
2025-11-08 21:35:48 +0545

    template-dashboard-add

--- app/Http/Controllers/backend/AdminController.php
+++ app/Http/Controllers/backend/AdminController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     //
      public function dashboard(){
-        return view('backend.admin.dashboard');
+        return view('backend.index');
+    }
+     public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+
+    public function login()
+    {
+        return view('backend.auth.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,52 @@
 <?php
 
+use App\Http\Controllers\admin\AdminProfileController;
 use App\Http\Controllers\backend\admin\DashboardController;
 use App\Http\Controllers\backend\AdminController;
+use App\Http\Controllers\backend\SettingController;
 use App\Http\Controllers\backend\UserController;
+use App\Http\Controllers\frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+Route::middleware(['auth', 'verified', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::post('/logout', [AdminController::class, 'destroy'])->name('logout');
+
+    /* Control Setting  */
+     Route::resource('setting', SettingController::class);
+
+    //  /*  control Profile */
+     Route::resource('/profile', AdminProfileController::class);
+
+
+});
+
+
+
+
+
+//user credentials
+
+//user Route
+
+Route::get('/dashboard', [FrontendController::class, 'dashboard'])->middleware(['auth', 'verified', 'roles:user'])->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'verified','role:user'])->prefix('user')->name('user.')->group(function(){
-    
-    Route::get('dashboard',[UserController::class,'dashboard'])->name('dashboard');
-    
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['role:admin','auth','verified'])->prefix('admin/')->name('admin.')->group(function(){
-    Route::get('dashboard',[AdminController::class,'dashboard'])->name('dashboard');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
